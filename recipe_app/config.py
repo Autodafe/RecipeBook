d211becfae64a4223f7f2261dbb547af46c82317
Autodafe <47718726+Autodafe@users.noreply.github.com>
2022-02-17 03:43:16 +0300

    Final version

--- recipe_app/config.py
+++ recipe_app/config.py
@@ -1,10 +1,10 @@
 class Config:
     # Данные конфигурации подключения к БД
     dialect = 'postgresql'
-    username = 'postgres'
-    password = 'Qwerty7'
-    host = 'localhost'
-    db_name = 'recipe_book'
+    username = 'vhreypnmdgorcg'
+    password = '2180c86ea96dd86a268846a2dda3ebd56654123ed12549dfbfc1ca30e0a6ccf6'
+    host = 'ec2-54-158-26-89.compute-1.amazonaws.com'
+    db_name = 'ddunej9gqkblss'
 
     # Настройки для экземляра: секретный ключ запуска и путь к БД
     SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
